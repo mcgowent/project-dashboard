6d2ac57ba550fc83e5557e737828aafdf7669730
Timothy McGowen <timothymcgowen@Timothys-MacBook-Air.local>
2021-06-07 18:19:04 -0700

    initial

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'project-dashboard-v1'`, () => {
+  it(`should have as title 'admin-panel-layout'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('project-dashboard-v1');
+    expect(app.title).toEqual('admin-panel-layout');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('project-dashboard-v1 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('admin-panel-layout app is running!');
   });
 });
